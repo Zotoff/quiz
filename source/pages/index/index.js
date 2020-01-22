ba33654ec78cc893e8470fa527c26e2b18616869
Pavel Zotov <paulzotoff@gmail.com>
2020-01-22 22:46:58 +0300

    Initial commit with webpack

---
+++ source/pages/index/index.js
@@ -0,0 +1,7 @@
+import createMenu from '../../components/menu/menu';
+import 'normalize.css';
+import './index.scss';
+let menu = createMenu(['Главная', 'Блог'], 'menu');
+document.body.appendChild(menu);
+
+console.log('Index.js');
